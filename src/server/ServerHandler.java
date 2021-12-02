d10c6c2ee9a6b9b4df04671e3611a25158da2684
GiaBaone <102190252@sv1.dut.udn.vn>
2021-12-02 16:43:35 +0700

    handle read file + percent to progess read file 2.12.21

--- src/server/ServerHandler.java
+++ src/server/ServerHandler.java
@@ -1,12 +1,15 @@
 package server;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import utils.FileUtils;
 import utils.Send_Type;
 
-public class ServerHandler {
+public class ServerHandler extends Thread {
 	private Socket socket;
 	private boolean isStop = false;
 	
@@ -19,6 +22,73 @@ public class ServerHandler {
 	Send_Type sendType = Send_Type.DO_NOT_SEND;
 	String message;
 	String filename;
+	
+	
+	FileWorker fileWorker;
+	private long fileSize;
+	private String fileNameReceived;
+	private long currentSize;
+	FileUtils utils;
+	
+	public ServerHandler(Socket soc, ISocketServerListener iSocketServerListener) throws IOException {
+		this.socket=soc;
+		os= soc.getOutputStream();
+		is = soc.getInputStream();
+		
+		this.iSocketServerListener= iSocketServerListener;
+		fileWorker= new FileWorker();
+		
+		utils = new FileUtils();
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Processing:  " + socket);
+		
+		while(!isStop) {
+			try {
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+		System.out.println("Complete processing: " + socket);
+	}
+	void readData() throws Exception{
+		try {
+			System.out.println("Receiving...");
+			ObjectInputStream ois = new ObjectInputStream(is);
+			Object obj = ois.readObject();
+			
+			if(obj instanceof String) {
+				readString(obj);
+			} else if (obj instanceof FileUtils) {
+				readFile(obj);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	private String readString (Object obj) {
+		String str = obj.toString();
+		
+		iSocketServerListener.showDialog(str, "STRING INFOR");
+		
+		return str;
+		
+	}
+	
+	private void readFile(Object obj) {
+		FileUtils fileUtils = (FileUtils) obj;
+		currentSize +=512;
+		
+		//count percent progess 50% ...
+		int percent =(int) (currentSize*100/fileSize);
+		
+		utils.appendByte(fileUtils.data);
+		iSocketServerListener.showProgessBarPercent(percent);
+	}
 	
 	
 }
